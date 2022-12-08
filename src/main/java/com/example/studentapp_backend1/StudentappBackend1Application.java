6101b9c1cbdf8d70bbbd431c06596d205eeffa72
SACHIN K V <sachin.kv.1011@gmail.com>
2022-12-08 21:20:57 +0530

    added

--- src/main/java/com/example/studentapp_backend1/StudentappBackend1Application.java
+++ src/main/java/com/example/studentapp_backend1/StudentappBackend1Application.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentappBackend1Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(StudentappBackend1Application.class,  args);
 	}
 
